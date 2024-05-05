3165e6e55075029288043a4ec4be28f78f5c8da8
maltemrts <149094617+maltemrts@users.noreply.github.com>
2024-05-05 14:29:17 +0200

    Change static numbers to ROW_COUNT and COL_COUNT

--- Board.java
+++ Board.java
@@ -5,8 +5,11 @@ public class Board {
 
     private JPanel panel;
 
+    private int ROWS_COUNT = 10;
+    private int COL_COUNT = 10;
+
     public Board() {
-        panel = new JPanel(new GridLayout(10, 10));
+        panel = new JPanel(new GridLayout(ROWS_COUNT, COL_COUNT));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         for (int i = 0; i < 10; i++) {
