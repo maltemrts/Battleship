9c0dd530e75c00f43eb8a624786cd7dddc623898
maltemrts <149094617+maltemrts@users.noreply.github.com>
2024-05-05 14:25:22 +0200

    Remove Comments

--- BattleGround.java
+++ BattleGround.java
@@ -8,17 +8,16 @@ public class BattleGround {
         Board board1 = new Board();
         Board board2 = new Board();
 
-        // Erstellen eines übergeordneten Containers
+
         JFrame frame = new JFrame("Battle Ground");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(1, 2)); // GridLayout mit einer Zeile und zwei Spalten
+        frame.setLayout(new GridLayout(1, 2)); 
 
-        // Hinzufügen der Kästchenfelder zum übergeordneten Container
+        
         frame.add(board1.getPanel());
         frame.add(board2.getPanel());
 
-        // Anpassen der Größe des Frames und Anzeigen
-        frame.setSize(1000, 500); // Breite verdoppelt, um Platz für beide Kästchenfelder zu schaffen
+        frame.setSize(1000, 500);
         frame.setVisible(true);
     }
 }
