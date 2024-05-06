e76c58de160b99a5a942676e15ed448158461872
maltemrts <149094617+maltemrts@users.noreply.github.com>
2024-05-06 18:26:55 +0200

    Change Code in Grid Layout

--- Board.java
+++ Board.java
@@ -8,7 +8,6 @@ import java.awt.event.MouseListener;
 public class Board {
 
     private JPanel panel;
-
     private int ROWS_COUNT = 10;
     private int COL_COUNT = 10;
 
@@ -16,17 +15,22 @@ public class Board {
         panel = new JPanel(new GridLayout(ROWS_COUNT, COL_COUNT));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                JPanel square = new JPanel();
-                square.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                square.addMouseListener(new MouseAdapter() {
+        JPanel[][] field = new JPanel[ROWS_COUNT][COL_COUNT];
+
+        for (int i = 0; i < ROWS_COUNT; i++) {
+            for (int j = 0; j < COL_COUNT; j++) {
+                field[i][j] = new JPanel();
+                field[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                final int row = i;
+                final int col = j;
+                field[i][j].addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        square.setBackground(Color.red);
+                        field[row][col].setBackground(Color.red);
+                        
                     }  
                 });
-                panel.add(square);
+                panel.add(field[i][j]);
             }
         }
 
