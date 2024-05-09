46e7948156689ec911d4cd577531f725d21a8340
Leo <150942688+L0uv15@users.noreply.github.com>
2024-05-09 11:06:40 +0000

    add attributes and methodes to the cell class

--- Cell.java
+++ Cell.java
@@ -1,3 +1,46 @@
 public class Cell {
-    
+    private int x;
+    private int y;
+    private boolean hit;
+    private boolean occupied;
+    private boolean status; // Der Status der Zelle (z.B. "getroffen" oder "nicht getroffen")
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.hit = false;
+        this.occupied = false;
+        this.status = false; // Zu Beginn ist die Zelle nicht getroffen
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isHit() {
+        return hit;
+    }
+
+    public void setHit(boolean hit) {
+        this.hit = hit;
+        this.status = true; // Setze den Status auf "getroffen", wenn die Zelle getroffen wird
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
 }
+
+
