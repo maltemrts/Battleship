5da723dcd02be8415a7f94c6eea08921faf14544
Leo <150942688+L0uv15@users.noreply.github.com>
2024-05-09 11:15:18 +0000

    add attributes and methodes to the ship class

--- Ship.java
+++ Ship.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Ship {
-    
+    private String shipName;
+    private int length;
+    private ArrayList<Cell> occupiedCells;
+
+    public Ship(String shipName, int length) {
+        this.shipName = shipName;
+        this.length = length;
+        this.occupiedCells = new ArrayList<>();
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public ArrayList<Cell> getOccupiedCells() {
+        return occupiedCells;
+    }
 }
+
